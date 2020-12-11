8d6505bd68cb360a3c16356c83dee9209cbbf022
Marta Pettersson <marta.pettersson@gmail.com>
2020-12-11 10:10:44 +0100

    Install bootstrap and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
+import React from "react";
+import { Switch, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <h1>test</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>test</h1>
+		</div>
+	);
 }
 
 export default App;
