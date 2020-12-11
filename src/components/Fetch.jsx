d1af0611620487ec7ff8506a837b278eee4d57a1
Marta Pettersson <marta.pettersson@gmail.com>
2020-12-11 11:59:46 +0100

    Add subName

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export default function Fetch({ name }) {
+export default function Fetch({ name, subName = "" }) {
 	const url =
 		"https://market-data-collector.firebaseio.com/market-collector.json";
 	const [marketData, setMarketData] = useState(null);
@@ -9,10 +9,13 @@ export default function Fetch({ name }) {
 			.then((res) => res.json())
 			.then((data) => setMarketData(data));
 	}, []);
+	if (name === "crypto") subName = "usd";
+	if (name === "currencies") subName = "sek";
+	if (name === "indexes") subName = "se";
 	return (
 		<>
 			{!marketData && <p>Loading</p>}
-			{marketData && console.log(marketData[name].usd)}
+			{marketData && console.log(marketData[name][subName])}
 		</>
 	);
 }
