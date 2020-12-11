f6a1e5503ea13e0b09461dcd2e38d30f4cef72f8
Marta Pettersson <marta.pettersson@gmail.com>
2020-12-11 11:53:25 +0100

    Fetch work in progress

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-export default function Fetch() {
+export default function Fetch({ name }) {
 	const url =
 		"https://market-data-collector.firebaseio.com/market-collector.json";
 	const [marketData, setMarketData] = useState(null);
@@ -9,11 +9,10 @@ export default function Fetch() {
 			.then((res) => res.json())
 			.then((data) => setMarketData(data));
 	}, []);
-	console.log(marketData);
 	return (
 		<>
 			{!marketData && <p>Loading</p>}
-			{marketData && <div>{Object.entries(marketData)}</div>}
+			{marketData && console.log(marketData[name].usd)}
 		</>
 	);
 }
--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Fetch from "../components/Fetch";
 
 export default function ListPage() {
+	const name = window.location.pathname.split("/")[2];
 	return (
 		<div>
-			<h2>List Page</h2>
+			<h2>{name}</h2>
+			<Fetch name={name} />
 		</div>
 	);
 }
