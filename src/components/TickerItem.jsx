27819847571f1aa791a00dd530cd1f89df67e2c9
rikardbjerkenius <rikard@hullo.se>
2020-12-14 16:47:40 +0100

    ticker item design

--- src/components/TickerItem.jsx
+++ src/components/TickerItem.jsx
@@ -6,17 +6,25 @@ export default function TickerItem({ value }) {
 
 	return (
 		<div>
-			
-			<h1>{value.name}</h1>
-			<h2>{value.ticker}</h2>
-			<h3>Price: {value.price}</h3>
 
-			{value.today === 0 || value.today === undefined ?  "" : <p>Today: {value.today}</p>}
-			{value.w1 === 0 || value.w1 === undefined ?  "" : <p>W1: {value.w1}</p>}
-			{value.mtd === 0 || value.mtd === undefined ?  "" : <p>MTD: {value.mtd}</p>}
-			{value.y3 === 0 || value.y3 === undefined ?  "" : <p>Y3: {value.y3}</p>}
-			{value.y5 === 0 || value.y5 === undefined ? "" : <p>Y5: {value.y5}</p>}
-			{value.ytd === 0 || value.ytd === undefined ? "" : <p>YTD: {value.ytd}</p>}
+			<h1 className="text-center mt-5">{value.name}</h1>
+			<h2 className="text-center mb-5">{value.ticker}</h2>
+
+
+			<div className="row text-dark mb-3 mr-1 ml-1 p-3 bg-white shadow rounded-lg">
+				<div className="col-6">
+					<p>Price: {value.price}</p>
+					{value.today === 0 || value.today === undefined ?  "" : <p>Today: {value.today}</p>}
+				</div>
+
+				<div className="col-6">
+					{value.w1 === 0 || value.w1 === undefined ?  "" : <p>W1: {value.w1}</p>}
+					{value.mtd === 0 || value.mtd === undefined ?  "" : <p>MTD: {value.mtd}</p>}
+					{value.y3 === 0 || value.y3 === undefined ?  "" : <p>Y3: {value.y3}</p>}
+					{value.y5 === 0 || value.y5 === undefined ? "" : <p>Y5: {value.y5}</p>}
+					{value.ytd === 0 || value.ytd === undefined ? "" : <p>YTD: {value.ytd}</p>}
+				</div>
+			</div>
 
 			{/* Buggar ibland på nollor */}
 			{/* {value.y3 && <p>Y3: {value.y3}</p>}
