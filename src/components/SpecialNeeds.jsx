855a8f61101b893f9319e868661c685eaacab89c
Marta Pettersson <marta.pettersson@gmail.com>
2020-12-17 12:14:21 +0100

    Style SpecialNeeds Component

--- src/components/SpecialNeeds.jsx
+++ src/components/SpecialNeeds.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import trump from "../specialNeeds/trump_2.jpg";
+import trump from "../specialNeeds/trump_3.jpg";
 import horn from "../specialNeeds/horn.mp3";
 
 export default function SpecialNeeds() {
 	return (
 		<div className="d-flex  justify-content-center ">
 			<div style={{ maxWidth: "400px" }}>
-				<img className="img-fluid " src={trump} alt="Trump" />
-				<audio controls className="w-100">
+				<img
+					style={{ marginTop: "3rem" }}
+					className="img-fluid "
+					src={trump}
+					alt="Trump"
+				/>
+				<audio controls autoPlay className="w-100">
 					<source src={horn} type="audio/mpeg" />
 				</audio>
 			</div>
